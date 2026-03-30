12e037ace0fc45419eb9b1378c1fdaad997a9a22
iamkvnn <23110119@studnet.hcmute.edu.vn>
2026-03-30 19:52:13 +0700

    chore: remove unused import

--- learning/src/main/java/com/cinx/learning/messaging/EnrollmentEventListener.java
+++ learning/src/main/java/com/cinx/learning/messaging/EnrollmentEventListener.java
@@ -2,7 +2,6 @@ package com.cinx.learning.messaging;
 
 import com.cinx.learning.messaging.event.EnrolledCourseEvent;
 import com.cinx.learning.service.learningProgress.ILearningProgressService;
-import com.cinx.learning.service.learningPath.ILearningPathService;
 import lombok.RequiredArgsConstructor;
 import org.springframework.amqp.rabbit.annotation.RabbitListener;
 import org.springframework.stereotype.Component;
